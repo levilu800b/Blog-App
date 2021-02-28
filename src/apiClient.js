c031f85ac51f401312810021ec5e9930f43ca62a
levilu800b <levilu800b@gmail.com>
2021-02-28 18:11:11 +0000

    heroku

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = 'http://localhost:3001/'
+const url = 'https://blog-express-api.herokuapp.com/'
 
 export class ApiClient {
     constructor(tokenProvider, logoutHandler) {
